1a3d6e9ffa3d6f1551bb8b09f468b7be579258e2
Tathagata206 <tathagata.1986@gmail.com>
2025-07-12 12:39:56 +0530

    list operations

---
+++ My_list.py
@@ -0,0 +1,18 @@
+my_list = [10, "hello", 3.14, True, "Python"]
+print(my_list)
+#Replace the 2nd element
+my_list[1]="Goodbye"
+print(my_list)
+#Append a new item
+my_list.append("Goodbye")
+print(my_list)
+#Count a similar item
+x=my_list.count("Goodbye")
+print(x)
+#Sorting the list
+my_list1 = [5.2,3,7,10,8]
+my_list1.sort()
+print(my_list1)
+#Reverse the list
+my_list.reverse()
+print(my_list)
